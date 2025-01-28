0d9dc74fa993f8c0427eecc849e4ef7a84e76678
Alejandro Lisón <2903429@alu.murciaeduca.es>
2025-01-28 16:15:16 +0100

    Add files via upload

--- Menu Principal/Menu/Mundo.gd
+++ Menu Principal/Menu/Mundo.gd
@@ -8,12 +8,10 @@ func _on_button_pressed():
 
 
 func _on_button_2_pressed():
+	
 	get_tree().change_scene_to_file("res://controles/controles.tscn")
 	
 
 
-func _on_button_3_pressed():
+func _on_button_3_pressed(): 
 	get_tree().change_scene_to_file("res://creditos/creditos.tscn")
-
-
-
