fa50a5c07b27dd30db5c687ea960b106370d940c
Alejandro Lisón <2903429@alu.murciaeduca.es>
2025-01-28 14:24:49 +0100

    Add files via upload

---
+++ Menu Principal/Menu/Mundo.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://controles/controles.tscn")
+	
+
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://creditos/creditos.tscn")
+
+
+
