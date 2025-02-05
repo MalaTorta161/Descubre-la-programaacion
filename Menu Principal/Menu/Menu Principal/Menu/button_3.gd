570bcdf054421212e6672ef18ba7258147afae7e
Alejandro Lisón <2903429@alu.murciaeduca.es>
2025-02-05 13:33:11 +0100

    Add files via upload

---
+++ Menu Principal/Menu/Menu Principal/Menu/button_3.gd
@@ -0,0 +1,24 @@
+extends Button
+@onready var audio_stream_player_5: AudioStreamPlayer = $AudioStreamPlayer5
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+
+
+
+
+
+func _on_button_down() -> void:
+	audio_stream_player_5.playing = true
+	pass # Replace with function body.
